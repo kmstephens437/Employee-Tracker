ea00f4b168421cc9e2b92b2e615977bd8e2e4fb4
Kevin-Stephens <kmstephens437@gmail.com>
2023-10-17 20:52:13 -0500

    creating tables in Schema cont.

--- db/schema.sql
+++ db/schema.sql
@@ -9,6 +9,14 @@ CREATE TABLE department (
 );
 CREATE TABLE role (
     id INT PRIMARY KEY NOT NULL,
-    title VARCHAR(30)
+    title VARCHAR(30),
+    salary DECIMAL 
+    department_id INT 
 );
-CREATE TABLE employee ();
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);

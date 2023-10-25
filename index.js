087f1979698c0e2828432eee2a5a4244b06baabb
Kevin-Stephens <kmstephens437@gmail.com>
2023-10-24 19:37:51 -0500

    installed dotenv and required.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:process.env.MYSQL_PASSWORD,
+    database: 'tracker_db'
+});
+
+
+
